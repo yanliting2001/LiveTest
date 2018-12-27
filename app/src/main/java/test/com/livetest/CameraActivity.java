8aeab20a1270ca1e4352af9a2671d95dde0ba72d
yugimaster <wei.yuan@zenithnine.com>
2018-12-27 12:22:49 +0800

    camera preview demo

--- app/src/main/java/test/com/livetest/CameraActivity.java
+++ app/src/main/java/test/com/livetest/CameraActivity.java
@@ -1,16 +1,10 @@
 package test.com.livetest;
 
-import android.app.Activity;
 import android.content.res.Configuration;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.github.faucamp.simplertmp.RtmpHandler;
@@ -22,25 +16,16 @@ import net.ossrs.yasea.SrsRecordHandler;
 
 import java.io.IOException;
 import java.net.SocketException;
-import java.util.List;
 
 /**
  * Created by Sikang on 2017/5/2.
  */
 
-public class CameraActivity extends Activity implements SrsEncodeHandler.SrsEncodeListener, RtmpHandler.RtmpListener, SrsRecordHandler.SrsRecordListener, View.OnClickListener {
+public class CameraActivity extends AppCompatActivity implements SrsEncodeHandler.SrsEncodeListener, RtmpHandler.RtmpListener, SrsRecordHandler.SrsRecordListener {
     private static final String TAG = "CameraActivity";
 
-    private Button mPublishBtn;
-    private Button mRecordBtn;
-    private Button mCameraSwitchBtn;
-    private Button mEncoderBtn;
-    private EditText mRempUrlEt;
     private SrsCameraView mCameraView;
     private SrsPublisher mPublisher;
-    private String rtmpUrl;
-    private String recPath = Environment.getExternalStorageDirectory().getPath() + "/test.mp4";
-    private Camera mCamera;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,21 +33,11 @@ public class CameraActivity extends Activity implements SrsEncodeHandler.SrsEnco
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_camera);
 
-        mPublishBtn = (Button) findViewById(R.id.publish);
-        mRecordBtn = (Button) findViewById(R.id.record);
-        mCameraSwitchBtn = (Button) findViewById(R.id.swCam);
-        mEncoderBtn = (Button) findViewById(R.id.swEnc);
-        mRempUrlEt = (EditText) findViewById(R.id.url);
-        mPublishBtn.setOnClickListener(this);
-        mRecordBtn.setOnClickListener(this);
-        mCameraSwitchBtn.setOnClickListener(this);
-        mEncoderBtn.setOnClickListener(this);
-
         mCameraView = (SrsCameraView) findViewById(R.id.glsurfaceview_camera);
         mPublisher = new SrsPublisher(mCameraView);
         //编码状态回调
         mPublisher.setEncodeHandler(new SrsEncodeHandler(this));
-        mPublisher.setRecordHandler(new SrsRecordHandler(this));
+//        mPublisher.setRecordHandler(new SrsRecordHandler(this));
         //预览分辨率
         mPublisher.setPreviewResolution(1280, 720);
         //推流分辨率
@@ -72,69 +47,6 @@ public class CameraActivity extends Activity implements SrsEncodeHandler.SrsEnco
         //打开摄像头，开始预览（未推流）
         mCameraView.setPreviewOrientation(0);
         mPublisher.startCamera();
-
-        mCamera = mPublisher.getCamera();
-        List<Camera.Size> sizeList = mCamera.getParameters().getSupportedVideoSizes();
-        for (int i = 0; i < sizeList.size(); i++) {
-            System.out.println("Supported video sizes: width is " + sizeList.get(i).width + ", height is " + sizeList.get(i).height);
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            //开始/停止推流
-            case R.id.publish:
-                if (mPublishBtn.getText().toString().contentEquals("开始")) {
-                    rtmpUrl = mRempUrlEt.getText().toString();
-                    if (TextUtils.isEmpty(rtmpUrl)) {
-                        Toast.makeText(getApplicationContext(), "地址不能为空！", Toast.LENGTH_SHORT).show();
-                    }
-                    //rtmp推流状态回调
-                    mPublisher.setRtmpHandler(new RtmpHandler(this));
-                    mPublisher.startPublish(rtmpUrl);
-
-                    if (mEncoderBtn.getText().toString().contentEquals("软编码")) {
-                        Toast.makeText(getApplicationContext(), "当前使用硬编码", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "当前使用软编码", Toast.LENGTH_SHORT).show();
-                    }
-                    mPublishBtn.setText("停止");
-                    mEncoderBtn.setEnabled(false);
-                } else if (mPublishBtn.getText().toString().contentEquals("停止")) {
-                    mPublisher.stopPublish();
-                    mPublisher.stopRecord();
-                    mPublishBtn.setText("开始");
-                    mEncoderBtn.setEnabled(true);
-                }
-                break;
-            //录制/结束
-            case R.id.record:
-                if (mRecordBtn.getText().toString().contentEquals("录制")) {
-                    mPublisher.startRecord(recPath);
-                    mRecordBtn.setText("结束");
-                    mEncoderBtn.setEnabled(false);
-                } else if (mRecordBtn.getText().toString().contentEquals("结束")) {
-                    mPublisher.stopRecord();
-                    mRecordBtn.setText("录制");
-                    mEncoderBtn.setEnabled(true);
-                }
-                break;
-            //切换摄像头
-            case R.id.swCam:
-                mPublisher.switchCameraFace((mPublisher.getCamraId() + 1) % Camera.getNumberOfCameras());
-                break;
-            //切换编码方式
-            case R.id.swEnc:
-                if (mEncoderBtn.getText().toString().contentEquals("软编码")) {
-                    mPublisher.switchToSoftEncoder();
-                    mEncoderBtn.setText("硬编码");
-                } else if (mEncoderBtn.getText().toString().contentEquals("硬编码")) {
-                    mPublisher.switchToHardEncoder();
-                    mEncoderBtn.setText("软编码");
-                }
-                break;
-        }
     }
 
     @Override
@@ -147,9 +59,6 @@ public class CameraActivity extends Activity implements SrsEncodeHandler.SrsEnco
     protected void onPause() {
         super.onPause();
         mPublisher.pauseRecord();
-        if (mRecordBtn.getText().toString().contentEquals("结束")) {
-            mRecordBtn.setText("录制");
-        }
     }
 
     @Override
@@ -165,9 +74,6 @@ public class CameraActivity extends Activity implements SrsEncodeHandler.SrsEnco
         mPublisher.stopEncode();
         mPublisher.stopRecord();
         mPublisher.setScreenOrientation(newConfig.orientation);
-        if (mPublishBtn.getText().toString().contentEquals("停止")) {
-            mPublisher.startEncode();
-        }
         mPublisher.startCamera();
     }
 
@@ -191,7 +97,6 @@ public class CameraActivity extends Activity implements SrsEncodeHandler.SrsEnco
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
             mPublisher.stopPublish();
             mPublisher.stopRecord();
-            mPublishBtn.setText("开始");
         } catch (Exception e1) {
             //
         }
